0ef57f04739c24ace2b0f3cd2a61276577ba92bd
Matt Ray <matthewhray@gmail.com>
2013-12-24 09:40:34 -0600

    Rubocop support for metadata.rb and Gemfile
    Replace double-quotes with single-quotes for now, expect this to act as a gate
    in the future.
    
    Change-Id: I10ff8b23517cb334905b162ce14059dbf23d7bc8

--- metadata.rb
+++ metadata.rb
@@ -1,24 +1,24 @@
-name             "openstack-block-storage"
-maintainer       "AT&T Services, Inc."
-maintainer_email "cookbooks@lists.tfoundry.com"
-license          "Apache 2.0"
-description      "The OpenStack Advanced Volume Management service Cinder."
+name             'openstack-block-storage'
+maintainer       'AT&T Services, Inc.'
+maintainer_email 'cookbooks@lists.tfoundry.com'
+license          'Apache 2.0'
+description      'The OpenStack Advanced Volume Management service Cinder.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "8.0.0"
+version          '8.0.0'
 
-recipe           "openstack-block-storage::common", "Defines the common pieces of repeated code from the other recipes"
-recipe           "openstack-block-storage::api", "Installs the cinder-api, sets up the cinder database, and cinder service/user/endpoints in keystone"
-recipe           "openstack-block-storage::keystone_registration", "Registers cinder service/user/endpoints in keystone"
-recipe           "openstack-block-storage::scheduler", "Installs the cinder-scheduler service"
-recipe           "openstack-block-storage::volume", "Installs the cinder-volume service and sets up the iscsi helper"
+recipe           'openstack-block-storage::common', 'Defines the common pieces of repeated code from the other recipes'
+recipe           'openstack-block-storage::api', 'Installs the cinder-api, sets up the cinder database, and cinder service/user/endpoints in keystone'
+recipe           'openstack-block-storage::keystone_registration', 'Registers cinder service/user/endpoints in keystone'
+recipe           'openstack-block-storage::scheduler', 'Installs the cinder-scheduler service'
+recipe           'openstack-block-storage::volume', 'Installs the cinder-volume service and sets up the iscsi helper'
 
 %w{ ubuntu fedora redhat centos suse }.each do |os|
   supports os
 end
 
-depends          "apt"
-depends          "openstack-common", "~> 8.0"
-depends          "openstack-identity", "~> 8.0"
-depends          "openstack-image", "~> 8.0"
-depends          "selinux"
-depends          "python"
+depends          'apt'
+depends          'openstack-common', '~> 8.0'
+depends          'openstack-identity', '~> 8.0'
+depends          'openstack-image', '~> 8.0'
+depends          'selinux'
+depends          'python'
