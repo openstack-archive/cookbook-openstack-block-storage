9cc96cd64e752f9df41cab21b727c7ab3a860877
Mark Vanderwiel <vanderwl@us.ibm.com>
2015-06-05 11:55:26 -0500

    Allow oslo rabbit heartbeat configuration
    In order to allow components to better handle and respond to mq failures,
    oslo has some heartbeat options that are useful.
    
    Change-Id: If21b39cb0c640fea0846f448100842374f97f550
    Partial-Bug: #1462438

--- spec/cinder_common_spec.rb
+++ spec/cinder_common_spec.rb
@@ -370,7 +370,9 @@ describe 'openstack-block-storage::cinder-common' do
           it 'has default RPC/AMQP options set' do
             [/^rpc_conn_pool_size=30$/,
              /^amqp_durable_queues=false$/,
-             /^amqp_auto_delete=false$/].each do |line|
+             /^amqp_auto_delete=false$/,
+             /^heartbeat_timeout_threshold=0$/,
+             /^heartbeat_rate=2$/].each do |line|
               expect(chef_run).to render_config_file(file.name).with_section_content('oslo_messaging_rabbit', line)
             end
           end
