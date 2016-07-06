fe77b8f6262626c0d92d4a8338075a59012c5c98
Samuel Cassiba <s@cassiba.com>
2016-07-06 09:18:39 -0700

    Style and lint fixes to support newer ChefDK
    Change-Id: I39f35c718e706b8f29c2b4d6d6cbd270b510183e
    Depends-On: I0a3872cd3a21c31b5e3a7b6190f911280e9ed909
    Depends-On: I34e4c7b29ff7c36c91245d90fb0b6897847cd22f
    (cherry picked from commit 67149f00ffcf6c0a19d381cf6d3f79e740dc81bd)

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,8 @@
 name 'openstack-block-storage'
 maintainer 'openstack-chef'
 maintainer_email 'openstack-dev@lists.openstack.org'
+issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
+source_url 'https://github.com/openstack/cookbook-openstack-block-storage' if respond_to?(:source_url)
 license 'Apache 2.0'
 description 'The OpenStack Advanced Volume Management service Cinder.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
