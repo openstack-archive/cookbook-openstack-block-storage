a29984d3012a119e5a2c2b8e292446940000916e
Jan Klare <j.klare@cloudbau.de>
2017-08-17 14:18:49 +0200

    starting pike development patch
    Change-Id: I8763df1d627bf840035fcde552bac3e9820ca913

--- metadata.rb
+++ metadata.rb
@@ -4,15 +4,15 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license          'Apache 2.0'
 description      'The OpenStack Advanced Volume Management service Cinder.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '15.0.0'
+version          '16.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
-depends 'openstack-common', '>= 15.0.0'
-depends 'openstack-identity', '>= 15.0.0'
-depends 'openstack-image', '>= 15.0.0'
+depends 'openstack-common', '>= 16.0.0'
+depends 'openstack-identity', '>= 16.0.0'
+depends 'openstack-image', '>= 16.0.0'
 depends 'openstackclient'
 
 depends 'apt', '~> 5.0'
